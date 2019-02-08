8e05ea4e18508d66554eb9cbb607fc03e081ccaa
Bernard <bchisumo74@gmail.com>
2019-02-08 17:32:09 +0000

    Testing Qeury Method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,9 +9,14 @@
             </h1>
 
             <?php
-                if ($database->connection){
-                    echo "True";
-                }
+
+            // Testing Query Method
+              $sql = "SELECT * FROM users WHERE id = 1";//as string
+              $result = $database->query($sql);
+              $user_found = mysqli_fetch_array($result);
+              echo $user_found['username']
+
+
             ?>
             <ol class="breadcrumb">
                 <li>
@@ -27,3 +32,7 @@
 
 </div>
 <!-- /.container-fluid -->
+
+<!--//                if ($database->connection){-->
+<!--//                    echo "True";-->
+<!--//                }-->
