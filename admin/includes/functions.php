00a32ce241b557ae35b5f3fb563f20f540ed24f7
Bernard <bchisumo74@gmail.com>
2019-02-12 20:38:02 +0000

    Controlling Access to Admin

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -18,10 +18,12 @@ function classAutoLoader($class){
 
 spl_autoload_register('classAutoLoader');
 
-//function redirect($location){
-//
-//    header("Location: {$location}");
-//}
+
+//Custom redirect
+function redirect($location){
+
+    header("Location: {$location}");
+}
 
 
 
