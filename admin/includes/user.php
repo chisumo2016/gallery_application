08fc69909339ed8c1feb56a82376b786d0f8fd6f
Bernard <bchisumo74@gmail.com>
2019-02-12 21:04:47 +0000

    Impelemented the Verify Method Part 2

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -85,6 +85,17 @@ class User
         //Sanitize our database
         $username  = $database->escape_string($username);
         $password  = $database->escape_string( $password);
+
+        $sql  =  "SELECT * FROM users  WHERE ";
+        $sql  .= "username = '{$username}' ";
+        $sql  .= "AND password = '{$password}' ";
+        $sql  .="LIMIT 1";
+
+        $the_result_array =self:: find_this_query($sql);
+
+        return !empty($the_result_array ) ? array_shift($the_result_array ): false;
+
+
     }
 }
 
