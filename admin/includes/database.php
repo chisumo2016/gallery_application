5b8297edf7c0220ad31f16b529d6e6a05f33c740
Bernard <bchisumo74@gmail.com>
2019-02-12 12:26:46 +0000

    Improving the connection into OPP -Database class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -5,6 +5,7 @@
 
 class Database {
 
+    //open connection
     public $connection;
 
     //Constructor
@@ -15,29 +16,27 @@ class Database {
     }
 
     public function  open_db_connection(){
-        $this->connection = mysqli_connect(
-            DB_HOST,
-            DB_USER,
-            DB_PASS,
-            DB_NAME,
-            DB_PORT);
+        //$this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
-        if (mysqli_connect_errno()){
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
-            die("Dtabase Connection failed badly" . mysqli_error());
+        if ($this->connection->connect_errno){  //mysqli_connect_errno()
+
+            die("Database Connection failed badly" . $this->connection->connect_error);
         }
     }
 
     // Query Method
 
     public function query($sql){
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);    //mysqli_query($this->connection, $sql);
+        $this->confirm_query( $result);
         return $result;
     }
 
     private  function  confirm_query($result){
         if (!$result){
-            die("Query Failed");
+            die("Query Failed" .$this->connection->error);
         }
 
     }
@@ -45,8 +44,14 @@ class Database {
     //Escape string
 
     public function  escape_string($string){
-        $escape_string = mysqli_real_escape_string($this->connection, $string);
+        $escape_string = $this->connection->real_escape_string($string);
         return $escape_string;
+    }
+
+    //Insert Id
+
+    public function  the_insert_id(){
+        return $this->connection->insert_id;
     }
 
 
