51b9f3b54668b339f25e16b64258beb35b37e8c4
Bernard <bchisumo74@gmail.com>
2019-02-12 16:09:06 +0000

    Created the Attribute to find Method in User Class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -37,7 +37,6 @@ class User
    //Auto Instantation Method
 
     public static  function  instantation($the_record){  // from database
-        //Assign Array value to object
 
         $the_object = new self;
 
@@ -45,12 +44,20 @@ class User
 
          foreach ($the_record as $the_attribute => $value) {
              if ($the_object->has_the_attribute($the_attribute)) {
-                 //Assign to Object attribute 
+                 //Assign to Object attribute
                  $the_object->the_attribute = $value;
              }
          }
 
         return $the_object;
+    }
+
+    private function  has_the_attribute($the_attribute) {
+         //Build in function -will return all property
+        $object_properties = get_object_vars($this);
+
+        //Find if the key exists
+        return array_key_exists($the_attribute, $object_properties);
     }
 }
 
