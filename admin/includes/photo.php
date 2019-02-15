6c2898e2070fa657c4818aab9f57503e63dcc974
Bernard <bchisumo74@gmail.com>
2019-02-15 16:09:15 +0000

    Adding Class Properties in Photo class which comes from DB

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -2,5 +2,17 @@
 
 
 Class Photo extends  Db_object {
+    //Properties
+
+    //Abstract Tables
+    protected  static  $db_table = "users";
+    protected  static  $db_table_fields = ['photo_id ','title','description','filename','type','size'];  // Column from database
+
+    public $photo_id ;
+    public $title ;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
 
 }
