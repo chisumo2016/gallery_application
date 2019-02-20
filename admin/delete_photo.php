835163a3ea37f85bb0c6624c7713e0d072a0cad4
Bernard <bchisumo74@gmail.com>
2019-02-20 15:26:35 +0000

    Delete Page and Links on photo P#1

---
+++ admin/delete_photo.php
@@ -0,0 +1,11 @@
+<?php include("includes/init.php"); ?>
+
+<!--//Accessing the Admin page-->
+<?php if (!$session->is_signed_in()){ redirect("login.php");} ?>
+
+<!--Process the Method for Delete-->
+
+<?php
+   echo "t";
+?>
+
