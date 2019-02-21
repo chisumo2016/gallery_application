eb6d8b7301fbc123b7cbaaee8915b8e4119d8a78
Bernard <bchisumo74@gmail.com>
2019-02-21 20:31:36 +0000

    Self Instantiation Comment Method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -18,7 +18,24 @@ class Comment  extends  Db_object
 
     //Method
 
-    
+    public  static  function create_comment($photo_id , $author="Joh Gon", $body=""){
+        //Check condition
+        if (!empty($photo_id) && !empty($author) && !empty($body)){
+            
+            //Instantiated the Object
+            $comments = new Comment();
+
+            //Assign the value to properties
+            $comments->photo_id = (int)$photo_id;
+            $comments->author   = $author;
+            $comments->body     = $body;
+
+            return $comments;
+        }else{
+
+            return false;
+        }
+    }
 
 
 
