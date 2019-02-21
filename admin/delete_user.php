5a5c4ff30ad32bf3c7d0409dbbd8f206e2d47044
Bernard <bchisumo74@gmail.com>
2019-02-21 14:40:06 +0000

    Delete Users Implemented Successfully

---
+++ admin/delete_user.php
@@ -0,0 +1,27 @@
+<?php include("includes/init.php"); ?>
+
+<!--//Accessing the Admin page-->
+<?php if (!$session->is_signed_in()){ redirect("login.php");} ?>
+
+<!--Detect the Id-->
+
+<?php
+  if (empty($_GET['id'])){
+      //Redirect
+      redirect("users.php");
+  }
+
+  //Instantiate
+$user = User::find_by_id($_GET['id']);
+
+  if ($user){
+
+      $user->delete();
+
+      redirect("users.php");
+
+  }else{
+      redirect("users.php");
+  }
+?>
+
