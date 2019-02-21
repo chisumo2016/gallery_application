e8833ce34d64d4ab082e92eea4e2ecb2aa8da5a9
Bernard <bchisumo74@gmail.com>
2019-02-21 20:20:26 +0000

    Creating the Comments Table in Database

--- admin/comments.php
+++ admin/comments.php
@@ -21,7 +21,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                     COMMENTS
+                        COMMENTS
                         <small>Subheading</small>
                     </h1>
                     <ol class="breadcrumb">
