5adb5b1e70ae6703789b400e7a3693636ad55f5b
Bernard <bchisumo74@gmail.com>
2019-02-26 16:15:19 +0000

    Implemented the Tracking Views Method in Session Class and Display in Dashboard

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -7,6 +7,7 @@ class Session{
     private $signed_in = false;
     public  $user_id;
     public  $message;
+    public  $count;
 
     //Constructor
     public function  __construct()
@@ -15,6 +16,7 @@ class Session{
         session_start();
         $this-> check_the_login();
         $this->check_message();
+        $this->visitor_count();
     }
 
     //Checking login Method
@@ -68,6 +70,16 @@ class Session{
         }else{
             $this->message = "";
         }
+    }
+
+    //Tracking via session
+    public  function  visitor_count(){
+        if (isset($_SESSION['count'])){
+            return $this->count = $_SESSION['count']++;
+        }else{
+            return  $_SESSION['count'] =1;
+        }
+
     }
 }
 
