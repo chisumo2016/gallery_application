abc7baae215c909408cf657af2936c9c21db3c71
Bernard <bchisumo74@gmail.com>
2019-02-26 15:57:10 +0000

    Build a Dashboard  with HTML

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -5,118 +5,111 @@
         <div class="col-lg-12">
             <h1 class="page-header">
                 ADMIN
-                <small>Subheading</small>
+                <small>Dashboard</small>
             </h1>
 
-            <?php
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-users fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">4</div>
+                                    <div>New Views</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-photo fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">7</div>
+                                    <div>Photos</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Photos in Gallery</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
 
-//            $user = User::find_by_id(1);
-//
-//            echo $user->username;
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">8
 
-              //Echo include Path
-            //echo DS;
-            //echo INCLUDES_PATH;
+                                    </div>
+
+                                    <div>Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Users</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-support fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">8</div>
+                                    <div>Comments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Comments</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
 
-
-
-               //PHOTO CLASS TESTING
-
-            //Display
-            /*$photos = Photo::find_all();
-            foreach ($photos as $photo){
-                echo $photo->filename;
-            }
-
-            //Create
-
-            $photo = new Photo();
-
-            $photo->title   = "Just Some Test";
-            $photo->description    = "something weared";
-            $photo->filename  = "image.png";
-            $photo->type    = "image";
-            $photo->size    = "13";
-
-            $photo->create();*/
-
-            /*
-             * //Testing the Parent Class
-             *
-                 $user = new User();
-                 $user->username = "NEW USER";
-                 $user->save();
-
-
-
-                 $users = User::find_all();
-                  foreach ($users as $user){
-                    echo $user->username;
-                }
-
-                 //Testing the Abstracted Update Method
-                    $user = User::find_user_by_id(5);
-                    $user->username = "Tanzania1";
-                    $user->password = "Tanzania1";
-                    $user->first_name = "Tanzania";
-                    $user->last_name = "Tanzania";
-                    $user->update();
-
-                 //Testing the Abstracted Create Method
-
-                 //Create
-
-                    $user = new User();
-
-                    $user->username    = "Sudent";
-                    $user->password    = "something weared";
-                    $user->first_name  = "SOL";
-                    $user->last_name    = "Don't Know";
-
-                    $user->create();
-             */
-
-            /*
-             //Update
-             $user = User::find_user_by_id(3);
-             $user->firstname = "Bernard1";
-             $user->lastname = "Bernard1";
-             $user->update();*/
-
-             /*
-                 //Delete
-
-            $user= User::find_user_by_id(6);
-            $user->delete();
-
-            //save
-
-            $user= User::find_user_by_id(5);
-            $user->password = "password";
-            $user->save();
-
-             //create
-            $user= new User();
-            $user->username = "Sue  200";
-            $user->save();
-            */
+            </div> <!--First Row-->
 
 
 
-
-
-            ?>
-
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> Blank Page
-                </li>
-            </ol>
         </div>
     </div>
     <!-- /.row -->
@@ -125,7 +118,108 @@
 
 
 
-
+<div class="tanza">
+<!--    --><?php
+//    //
+//    //
+//    ////            $user = User::find_by_id(1);
+//    ////
+//    ////            echo $user->username;
+//    //
+//    ////Echo include Path
+//    ////echo DS;
+//    ////echo INCLUDES_PATH;
+//    //
+//    //
+//    //
+//    //
+//    ////PHOTO CLASS TESTING
+//    //
+//    ////Display
+//    ///*$photos = Photo::find_all();
+//    //foreach ($photos as $photo){
+//    //    echo $photo->filename;
+//    //}
+//    //
+//    ////Create
+//    //
+//    //$photo = new Photo();
+//    //
+//    //$photo->title   = "Just Some Test";
+//    //$photo->description    = "something weared";
+//    //$photo->filename  = "image.png";
+//    //$photo->type    = "image";
+//    //$photo->size    = "13";
+//    //
+//    //$photo->create();*/
+//    //
+//    ///*
+//    // * //Testing the Parent Class
+//    // *
+//    //     $user = new User();
+//    //     $user->username = "NEW USER";
+//    //     $user->save();
+//    //
+//    //
+//    //
+//    //     $users = User::find_all();
+//    //      foreach ($users as $user){
+//    //        echo $user->username;
+//    //    }
+//    //
+//    //     //Testing the Abstracted Update Method
+//    //        $user = User::find_user_by_id(5);
+//    //        $user->username = "Tanzania1";
+//    //        $user->password = "Tanzania1";
+//    //        $user->first_name = "Tanzania";
+//    //        $user->last_name = "Tanzania";
+//    //        $user->update();
+//    //
+//    //     //Testing the Abstracted Create Method
+//    //
+//    //     //Create
+//    //
+//    //        $user = new User();
+//    //
+//    //        $user->username    = "Sudent";
+//    //        $user->password    = "something weared";
+//    //        $user->first_name  = "SOL";
+//    //        $user->last_name    = "Don't Know";
+//    //
+//    //        $user->create();
+//    // */
+//    //
+//    ///*
+//    // //Update
+//    // $user = User::find_user_by_id(3);
+//    // $user->firstname = "Bernard1";
+//    // $user->lastname = "Bernard1";
+//    // $user->update();*/
+//    //
+//    ///*
+//    //    //Delete
+//    //
+//    //$user= User::find_user_by_id(6);
+//    //$user->delete();
+//    //
+//    ////save
+//    //
+//    //$user= User::find_user_by_id(5);
+//    //$user->password = "password";
+//    //$user->save();
+//    //
+//    ////create
+//    //$user= new User();
+//    //$user->username = "Sue  200";
+//    //$user->save();
+//    //*/
+//    //
+//    //
+//    //
+//    //
+//    //
+//    //?><!-- -->
+</div>
 
 
 
