44e35e6717fc85ad43572cc616239ddc9c6faf46
Bernard <bchisumo74@gmail.com>
2019-02-26 17:30:16 +0000

    Implemented Dynamic Data in Chart Creation in footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -20,11 +20,11 @@
 
           var data = google.visualization.arrayToDataTable([
               ['Task', 'Hours per Day'],
-              ['Views',     <?php echo $session->count;?>],
-              ['Photo',      2],
-              ['Users',  2],
-              ['Comments', 2]
-//              ['Sleep',    7]
+              ['Views',      <?php echo $session->count;?>],
+              ['Comments',      <?php echo Comment::count_all();?>],
+              ['Users',      <?php echo User::count_all();?>],
+              ['Photos',   <?php echo Photo::count_all();?>]
+
           ]);
 
           var options = {
