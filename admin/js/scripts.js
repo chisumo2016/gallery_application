a2727e4c27314a115d1430b6aa1495d02e18a95f
Bernard <bchisumo74@gmail.com>
2019-02-27 11:53:26 +0000

    Photo Library Sidebar Implemented in script and ajax code file

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     var image_src;
     var image_href_splitted;
     var image_name;
+    var photo_id;
 
 
 
@@ -29,6 +30,11 @@ $(document).ready(function () {
         image_name = image_href_splitted[image_href_splitted.length - 1];
         //alert( image_name);
 
+        //Photo attribute Library Sidebar
+        photo_id = $(this).attr("data");
+
+
+
 
     });
 
@@ -48,6 +54,20 @@ $(document).ready(function () {
                 }
             }
         });
+    });
+
+
+    $.ajax({
+        url: "includes/ajax_code.php",
+        data:{photo_id:photo_id},
+        type: "POST",
+        success:function(data) {
+            if(!data.error) {
+
+                $("#modal_sidebar").html(data);
+            }
+        }
+
     });
 
 
