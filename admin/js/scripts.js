41ee198b456de5773eaac343115034a6b87e1641
Bernard <bchisumo74@gmail.com>
2019-02-27 10:31:00 +0000

    Implementing the Ajax -Setting User Image

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var user_id;
     var image_src;
     var image_href_splitted;
-    var image_id;
+    var image_name;
 
 
 
@@ -14,8 +14,10 @@ $(document).ready(function () {
     $(".modal_thumbnails").click(function () {
 
         $("#set_user_image").prop('disabled', false);
+
         //user_id to send over server  //Pulling User Id with jQuery and Javascript
-       user_href = $("#user-id").prop('href');
+        $(this).addClass('selected');
+        user_href = $("#user-id").prop('href');
          //slip
         user_href_splitted =user_href.split("=");
         user_id = user_href_splitted[user_href_splitted.length - 1];
@@ -24,10 +26,26 @@ $(document).ready(function () {
 
         image_src  = $(this).prop("src");
         image_href_splitted =image_src.split("/");
-        image_id = image_href_splitted[image_href_splitted.length - 1];
-        alert( image_id);
+        image_name = image_href_splitted[image_href_splitted.length - 1];
+        alert( image_name);
 
 
+    });
+
+   // Ajax -Setting User Image
+    $("#set_user_image").click(function () {
+        
+        $.ajax({
+
+            url: "includes/ajax_code.php",
+            data:{image_name: image_name, user_id:user_id},
+            type:"POST",
+            success:function (data) {
+                if (!data.error){
+                    alert(image_name);
+                }
+            }
+        });
     });
 
 
