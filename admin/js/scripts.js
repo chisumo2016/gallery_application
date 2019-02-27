d37ad6b3899cc0f583b5e7555da4da5fef4bd9ae
Bernard <bchisumo74@gmail.com>
2019-02-27 10:16:35 +0000

    Pulling Image Name  with jQuery and Javascript

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,11 +1,16 @@
 
 $(document).ready(function () {
+    //properties
     var user_href;
     var user_href_splitted;
     var user_id;
+    var image_src;
+    var image_href_splitted;
+    var image_id;
 
 
-    // Enbabling Selection Button with Jquery
+
+    // Enabling Selection Button with Jquery
     $(".modal_thumbnails").click(function () {
 
         $("#set_user_image").prop('disabled', false);
@@ -15,8 +20,12 @@ $(document).ready(function () {
         user_href_splitted =user_href.split("=");
         user_id = user_href_splitted[user_href_splitted.length - 1];
 
+        //Pulling Image Name with Jquery and Javascript
 
-        alert( user_id);
+        image_src  = $(this).prop("src");
+        image_href_splitted =image_src.split("/");
+        image_id = image_href_splitted[image_href_splitted.length - 1];
+        alert( image_id);
 
 
     });
