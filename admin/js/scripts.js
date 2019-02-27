2af25fbf8836f7b4ca134bcf406cc14aed627dab
Bernard <bchisumo74@gmail.com>
2019-02-27 09:15:15 +0000

    Enabling Rthe Selection Button with Jquery

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-    
+    // Enbabling Selection Button with Jquery
+    $(".modal_thumbnails").click(function () {
+
+        $("#set_user_image").prop('disabled', false);
+        //user_id to send over server
+
+
+    });
 
 
 
